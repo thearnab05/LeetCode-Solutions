c0975bd656354bf81a4ec8dd3bbc84d3b8b63350
thearnab05 <arnabsural1890@gmail.com>
2026-03-16 10:46:15 +0530

    Implement strStr function to find the first occurrence of a substring

---
+++ FindOccurenceString.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+        if(n == 0){
+            return 0;
+        }
+
+        if(n > m){
+            return -1;
+        }
+
+        for(int i = 0; i <= m - n; i++){
+            if(haystack.charAt(i) == needle.charAt(0)){
+                if(haystack.substring(i, i + n).equals(needle)){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}

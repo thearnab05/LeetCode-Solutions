e2b331f994c8096baf4e4a8b0ad99f01a8526bde
thearnab05 <arnabsural1890@gmail.com>
2026-05-04 21:27:16 +0530

    Implement isValid method for validating parentheses using a stack

--- ValidParenthesis.java
+++ ValidParenthesis.java
@@ -1,3 +1,24 @@
-public class ValidParenthesis {
-    
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
